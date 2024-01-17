ab31606c20ceb9036029462e85c716ce9502d930
krongkan <krongkan.madjaroensuk@ibm.com>
2024-01-17 15:34:16 +0700

    add navbar

---
+++ src/config/index.ts
@@ -0,0 +1,46 @@
+export const PRODUCT_CATEGORIES = [
+    {
+        label: "UI Kits",
+        value: "ui_kits" as const,
+        featured: [
+            {
+                name: "Editor pick",
+                href: "#",
+                imageSrc: '/nav/ui-kits/mixed.jpg',
+            },
+            {
+                name: "new  arrivals",
+                href: "#",
+                imageSrc: '/nav/ui-kits/blue.jpg',
+            },
+            {
+                name: "best seller",
+                href: "#",
+                imageSrc: '/nav/ui-kits/purple.jpg',
+            },
+
+        ]
+    },
+    {
+        label: "Icons",
+        value: "icons" as const,
+        featured: [
+            {
+                name: "Favirite Icon Picks",
+                href: "#",
+                imageSrc: '/nav/icons/picks.jpg',
+            },
+            {
+                name: "new  arrivals",
+                href: "#",
+                imageSrc: '/nav/icons/new.jpg',
+            },
+            {
+                name: "best seller icon",
+                href: "#",
+                imageSrc: '/nav/icons/bestsellers.jpg',
+            },
+
+        ]
+    },
+]

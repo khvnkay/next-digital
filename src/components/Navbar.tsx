bd3c8d45e339646bfa62e5f8ff92530434192047
krongkan <krongkan.madjaroensuk@ibm.com>
2024-01-18 10:09:13 +0700

    add cart

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,10 +3,13 @@ import MaxWidthWrapper from './MaxWidthWrapper'
 import Link from 'next/link'
 import { Icons } from './Icons'
 import NavItems from './NavItems'
+import { buttonVariants } from './ui/button'
+import Cart from './Cart'
 
 type Props = {}
 
 export default function Navbar({ }: Props) {
+    const user = null
     return (
         <div className='bg-white sticky z-50 top-0 inset-x-0 h-16'>
             <header className='relative bg-white'>
@@ -21,6 +24,71 @@ export default function Navbar({ }: Props) {
 
                             <div className='hidden z-50 lg:ml-8 lg:block lg:self-stretch'>
                                 <NavItems />
+
+                            </div>
+
+                            <div className='ml-auto flex items-center'>
+                                <div className='hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6'>
+                                    {
+                                        user ? null : (
+                                            <Link href='/sign-in'
+                                                className={buttonVariants({ variant: 'ghost' })}
+
+
+                                            >
+                                                Sign in
+                                            </Link>
+                                        )
+                                    }
+
+                                    {
+                                        user ? null :
+
+                                            (
+                                                <span className='h-6 w-px bg-gray-200' aria-hidden="true">
+
+                                                </span>
+                                            )
+                                    }
+
+                                    {
+                                        user ? (
+                                            <p>
+                                            </p>
+                                        ) :
+                                            (
+                                                <Link href='/sign-up'
+                                                    className={buttonVariants({ variant: 'ghost' })}
+
+
+                                                >
+                                                    Sign up
+                                                </Link>
+                                            )
+                                    }
+                                    {
+                                        user ?
+                                            (
+                                                <span className='h-6 w-px bg-gray-200' aria-hidden="true">
+
+                                                </span>
+                                            ) : null
+                                    }
+                                    {
+                                        user ? null : <div className='flex lg:ml-6'>
+                                            <span className='h-6 w-px bg-gray-200' aria-hidden="true">
+
+                                            </span>
+
+                                        </div>
+                                    }
+
+                                    <div className='ml-4 flow-root lg:ml-6'>
+                                        <Cart />
+
+                                    </div>
+
+                                </div>
 
                             </div>
                         </div>
